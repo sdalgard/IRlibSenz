10fb6cdadd29209ca4360c83ffc2d0cc7bae385c
sdalgard <steffen.sint@gmail.com>
2017-02-11 23:44:01 +0100

    Moved decoding of the rotencoder to timer interrupt

--- irlibsenz/irlibsenz.ino
+++ irlibsenz/irlibsenz.ino
@@ -105,40 +105,47 @@ void send_command(int cmd_char, int repeat = 0) {
   //  mySender.send(0x00FF40BF); //AnalysIR Batch Export (IRLib) // AnalysIR IR Protocol: NEC, Key:  lys}
 }
 
+//#define TICK_PRELOAD 34286            // preload timer 65536-16MHz/256/2Hz
+//#define TICK_PRELOAD 62411             // preload timer 65536-16MHz/256/20Hz
+#define TICK_PRELOAD 65223             // preload timer 65536-16MHz/256/200Hz
 
+void initTimerTick() {
+  // initialize timer1 
+  noInterrupts();           // disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
 
+  TCNT1 = TICK_PRELOAD;
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();             // enable all interrupts
+}
+
+volatile int tickCnt = 0;
 int inChar;
-int lastEncode = -1;
 typedef enum {UP, DOWN, UNDEF} direction_t;
+int lastEncode = -1;
 direction_t lastDirection = UNDEF;
 int volCount = 0;
+int sentVolCount = 0;
 const byte encodeAPin = 2;
 const byte encodeBPin = 4;
 volatile byte state = LOW;
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(115200);
-  pinMode(encodeAPin, INPUT_PULLUP);
-  pinMode(encodeBPin, INPUT_PULLUP);
-} // the setup function runs once when you press reset or power the board
-
-// the loop routine runs over and over again forever:
-void loop() {
+ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defined function supplied by attachInterrupt
+{
   int newEncode = 0;
   direction_t newDirection = UNDEF;
   
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    inChar = Serial.read();
-    send_command(inChar);
-  }
-  
+  TCNT1 = TICK_PRELOAD;            // preload timer
+  //tickCnt++;
+
   newEncode |= digitalRead(encodeAPin); 
   newEncode <<=1;
   newEncode |= digitalRead(encodeBPin); 
+
   if (lastEncode != newEncode) {
+    //printf_serial("New encode %d %d vol %d\n", newEncode, lastEncode, volCount);
     
     switch (lastEncode) {
       case 0:
@@ -182,22 +189,59 @@ void loop() {
         }
         break;
     }
+    //printf_serial("New encode %d %d dir %d %d\n", newEncode, lastEncode, newDirection, lastDirection);
     if (newDirection != UNDEF) {
       if (lastDirection == newDirection) {
         if (newDirection == UP) {
-          volCount +=3;
-          send_command('+', 2);
+          volCount +=1;
         } else {
-          volCount -=3;
-          send_command('-', 2);
+          volCount -=1;
         }
         lastDirection = UNDEF;
-        printf_serial("New encode %d vol %d\n", newEncode, volCount);
+        //printf_serial("New encode %d vol %d\n", newEncode, volCount);
       } else {
         lastDirection = newDirection;
       }
     }
     lastEncode = newEncode;
+  }
+}
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(115200);
+  pinMode(encodeAPin, INPUT_PULLUP);
+  pinMode(encodeBPin, INPUT_PULLUP);
+  initTimerTick();
+} // the setup function runs once when you press reset or power the board
+
+// the loop routine runs over and over again forever:
+void loop() {
+  static int newTick = 0;
+  
+  if (newTick != tickCnt) {
+    newTick = tickCnt;
+    printf_serial("New tick %d \n", newTick);
+  }
+  
+  if (sentVolCount != volCount) {
+    int diffCount = volCount - sentVolCount;
+    if (diffCount > 0) {
+      send_command('+', diffCount-1);
+      sentVolCount += diffCount;
+    } else if (diffCount < 0) {
+      send_command('-', (-diffCount)-1);
+      sentVolCount += diffCount;
+    }
+    printf_serial("New volCount %d \n", volCount);
+  }
+  
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    inChar = Serial.read();
+    send_command(inChar);
   }
 }
 
