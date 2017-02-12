677fbd8d87ef0016377a32808593f53d7cc5b5d2
sdalgard <steffen.sint@gmail.com>
2017-02-12 20:51:02 +0100

    Added read of switches Ready for live test.

--- irlibsenz/irlibsenz.ino
+++ irlibsenz/irlibsenz.ino
@@ -22,62 +22,62 @@ void send_command(int cmd_char, int repeat = 0) {
   int   repeat_time = 150;
   switch (cmd_char) {
     case '1':
-      printf_serial("Send 1\n");
+      printf_serial("1\n");
       cmd_code = 0x00FF906F;
       break;
     case '2':
-      printf_serial("Send 2\n");
+      printf_serial("2\n");
       cmd_code = 0x00FFB847;
       break;
     case '3':
-      printf_serial("Send 3\n");
+      printf_serial("3\n");
       cmd_code = 0x00FFF807;
       break;
     case '4':
-      printf_serial("Send 4\n");
+      printf_serial("4\n");
       cmd_code = 0x00FF9867;
       break;
     case '5':
-      printf_serial("Send 5\n");
+      printf_serial("5\n");
       cmd_code = 0x00FFD827;
       break;
     case '6':
-      printf_serial("Send 6\n");
+      printf_serial("6\n");
       cmd_code = 0x00FFB24D;
       break;
     case '7':
-      printf_serial("Send 7\n");
+      printf_serial("7\n");
       cmd_code = 0x00FF28D7;
       break;
     case '8':
-      printf_serial("Send 8\n");
+      printf_serial("8\n");
       cmd_code = 0x00FF12ED;
       break;
     case '9':
-      printf_serial("Send 9\n");
+      printf_serial("9\n");
       cmd_code = 0x00FF2AD5 ;
       break;
     case 'o':
-      printf_serial("Send onoff\n");
+      printf_serial("onoff\n");
       cmd_code = 0x00FF08F7;
       delay_time = 6000;
       break;
     case '+':
-      printf_serial("Send +\n");
+      printf_serial("+\n");
       cmd_code = 0x00FF00FF;
       repeat_time = 100;
       break;
     case '-':
-      printf_serial("Send -\n");
+      printf_serial("-\n");
       cmd_code = 0x00FF58A7;
       repeat_time = 100;
       break;
     case 'a':
-      printf_serial("Send Alarm\n");
+      printf_serial("Alarm\n");
       cmd_code = 0x00FFC03F;
       break;
     case 'i':
-      printf_serial("Send Info\n");
+      printf_serial("Info\n");
       cmd_code = 0x00FF6897;
       break;
   }
@@ -126,10 +126,16 @@ int inChar;
 typedef enum {UP, DOWN, UNDEF} direction_t;
 int lastEncode = -1;
 direction_t lastDirection = UNDEF;
-int volCount = 0;
-int sentVolCount = 0;
-const byte encodeAPin = 2;
-const byte encodeBPin = 4;
+volatile int volCount = 0;
+const byte encodeAPin = 5;
+const byte encodeBPin = 6;
+const byte onOffPin = 7;
+const byte p1Pin = 13;
+const byte p2Pin = 12;
+const byte p3Pin = 11;
+const byte p4Pin = 10;
+const byte p5Pin = 9;
+const byte p6Pin = 8;
 volatile byte state = LOW;
 
 ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defined function supplied by attachInterrupt
@@ -207,6 +213,32 @@ ISR(TIMER1_OVF_vect)        // interrupt service routine that wraps a user defin
   }
 }
 
+int readPin(int pinNum) {
+  int ret = 0;
+  if (digitalRead(pinNum)==0) ret |= (1 << pinNum);
+  return ret; 
+}
+
+int readPins(void) {
+  int ret = 0;
+  ret |= readPin(onOffPin);
+  ret |= readPin(p1Pin);
+  ret |= readPin(p2Pin);
+  ret |= readPin(p3Pin);
+  ret |= readPin(p4Pin);
+  ret |= readPin(p5Pin);
+  ret |= readPin(p6Pin);
+  return ret;
+}
+
+void testPin(int pinNum, int cmd, int currXor) {
+  int pinMask = 1<<pinNum;
+  if ((currXor & pinMask) != 0) {
+    printf_serial("Do <%c> \n", cmd);
+    send_command(cmd);    
+  }
+}
+
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -214,28 +246,44 @@ void setup() {
   Serial.begin(115200);
   pinMode(encodeAPin, INPUT_PULLUP);
   pinMode(encodeBPin, INPUT_PULLUP);
+  pinMode(onOffPin, INPUT_PULLUP);
+  pinMode(p1Pin, INPUT_PULLUP);
+  pinMode(p2Pin, INPUT_PULLUP);
+  pinMode(p3Pin, INPUT_PULLUP);
+  pinMode(p4Pin, INPUT_PULLUP);
+  pinMode(p5Pin, INPUT_PULLUP);
+  pinMode(p6Pin, INPUT_PULLUP);
   initTimerTick();
 } // the setup function runs once when you press reset or power the board
 
 // the loop routine runs over and over again forever:
 void loop() {
   static int newTick = 0;
+  static int lastPins = 0;
+  int currPins = 0;
+  int currXorPins = 0;
   
   if (newTick != tickCnt) {
     newTick = tickCnt;
     printf_serial("New tick %d \n", newTick);
   }
   
-  if (sentVolCount != volCount) {
-    int diffCount = volCount - sentVolCount;
+  if (volCount != 0) {
+    int diffCount = volCount;
     if (diffCount > 0) {
+      if (diffCount > 2) diffCount = 2;
       send_command('+', diffCount-1);
-      sentVolCount += diffCount;
+      noInterrupts();           // disable all interrupts
+      volCount -= diffCount;
+      interrupts();             // enable all interrupts
     } else if (diffCount < 0) {
+      if (diffCount < -2) diffCount = -2;
       send_command('-', (-diffCount)-1);
-      sentVolCount += diffCount;
+      noInterrupts();           // disable all interrupts
+      volCount -= diffCount;
+      interrupts();             // enable all interrupts
     }
-    printf_serial("New volCount %d \n", volCount);
+    //printf_serial("New volCount %d \n", volCount);
   }
   
   if (Serial.available() > 0) {
@@ -243,6 +291,23 @@ void loop() {
     inChar = Serial.read();
     send_command(inChar);
   }
+  
+  currPins = readPins();
+  delay(20); // Delay 20ms to debounce 
+  currPins &= readPins();
+  
+  currXorPins = currPins ^ lastPins; // Which pins are changed
+  currXorPins &= currPins; // Which pins changed to 1
+  lastPins = currPins;
+  
+  testPin(onOffPin, 'o', currXorPins);
+  testPin(p1Pin, '1', currXorPins);
+  testPin(p2Pin, '2', currXorPins);
+  testPin(p3Pin, '3', currXorPins);
+  testPin(p4Pin, '4', currXorPins);
+  testPin(p5Pin, '5', currXorPins);
+  testPin(p6Pin, '6', currXorPins);
+  
 }
 
 
